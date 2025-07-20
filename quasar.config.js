e3672bb863c39142a3024d0ed5007614378c16cd
cmarrero2021 <marrero.c@gmail.com>
2025-07-20 15:39:50 -0400

    20250720_1520 Commit inicial

---
+++ quasar.config.js
@@ -0,0 +1,76 @@
+// quasar.config.js
+const { configure } = require('quasar/wrappers')
+
+module.exports = configure(function (/* ctx */) {
+  return {
+    eslint: {
+      warnings: true,
+      errors: true,
+    },
+
+    boot: [
+      // 'axios'
+    ],
+
+    css: ['app.scss'],
+
+    extras: ['roboto-font', 'material-icons'],
+
+    build: {
+      target: {
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+        node: 'node16',
+      },
+
+      vueRouterMode: 'hash',
+    },
+
+    devServer: {
+      open: true,
+    },
+
+    framework: {
+      config: {},
+
+      plugins: ['Notify', 'Loading', 'Dialog'],
+    },
+
+    animations: [],
+
+    ssr: {
+      pwa: false,
+      prodPort: 3000,
+      middlewares: ['render'],
+    },
+
+    pwa: {
+      workboxMode: 'generateSW',
+      injectPwaMetaTags: true,
+      swFilename: 'sw.js',
+      manifestFilename: 'manifest.json',
+      useCredentialsForManifestTag: false,
+    },
+
+    cordova: {},
+
+    capacitor: {
+      hideSplashscreen: true,
+    },
+
+    electron: {
+      inspectPort: 5858,
+
+      bundler: 'packager',
+
+      packager: {},
+
+      builder: {
+        appId: 'pivot-table-app',
+      },
+    },
+
+    bex: {
+      contentScripts: ['my-content-script'],
+    },
+  }
+})
